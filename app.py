26287e8402487f32778173d79e47ce452324904a
Nizamph <116118003+Nizamph@users.noreply.github.com>
2024-01-26 22:48:20 +0530

    created endpoints for getting and submitting expenses

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
+from pickle import BYTEARRAY8
 from flask import Flask
 import flask
 import json
 
 import datetime
 from datetime import date, timedelta, timezone
-from flask import jsonify, request, send_file
+from flask import jsonify, request, send_file,render_template,url_for,redirect
 from sqlalchemy import exists, select
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, get_jwt, \
     unset_jwt_cookies
 
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 from sqlalchemy.orm import aliased
+import os
 
 app = Flask(__name__)
 CORS(app)
+CORS(app, support_credentials=True)
 # cors = CORS(app, resources={r"/gettutoruser/*": {"origins": "http://localhost:3000"}})
 
 app.config["JWT_SECRET_KEY"] = "sdadwkhkln2313nklne3123"  # Change this!
@@ -23,7 +27,7 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
 
 jwt = JWTManager(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin''@localhost:3306/ordermodule'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:nizam@localhost:3306/newschema'
 
 # app.app_context().push()
 db = SQLAlchemy(app)
@@ -135,6 +139,18 @@ class Invoice(db.Model):
     client = db.relationship('Client', backref=db.backref('invoice'))
     order = db.relationship('Orders', backref=db.backref('orders_for_invoice'))
 
+class Expense(db.Model):
+    __tablename__ = 'expense'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    expense_date = db.Column(db.Date)
+    expense_number = db.Column(db.String(50))
+    invoice_number = db.Column(db.String(50),unique=True)
+    currency = db.Column(db.String(45))
+    amount = db.Column(db.Float)
+    notes = db.Column(db.String(255))
+    vendor = db.Column(db.String(45))
+    paid_amount = db.Column(db.Float)
+    due_date = db.Column(db.Date)
 
 # -------------------LOG in Module Route start -------------------__
 
@@ -161,7 +177,6 @@ def refresh_expiring_jwts(response):
 def test():
     email = flask.request.form['email']
     password = flask.request.form['password']
-
     user = Users.query.filter_by(email=email, password=password).first()
 
     if user:
@@ -178,7 +193,6 @@ def getAllInvoices():
         invoice_dict = {}
 
         # Iterate over students and their invoices
-        
         get_invoices = Invoice.query.all()
         for invoice in get_invoices:
             # Store the invoice in the dictionary using the invoice number as the key
@@ -205,6 +219,70 @@ def getAllInvoices():
     except Exception as e:
         print(e)
         return {"error": str(e)}
+
+
+
+from flask import request, jsonify
+
+@app.route('/submitexpense', methods=['POST'])
+@jwt_required()
+def submit_expense():
+        
+        data = flask.request.get_json()
+
+        expense_date = data.get('expense_date')
+        expense_number = data.get('expense_number')
+        invoice_number = data.get('invoice_number')
+        amount = data.get('amount')
+        notes = data.get('notes')
+        vendor = data.get('vendor')
+        currency = data.get('currency')
+
+        expense = Expense(
+            expense_date=expense_date,
+            expense_number=expense_number,
+            invoice_number=invoice_number,
+            currency=currency,
+            amount=amount,
+            notes=notes,
+            vendor=vendor,
+        )
+        db.session.add(expense)
+        db.session.commit()
+
+        return jsonify({'message': 'Expense submitted successfully'})
+
+
+@app.route('/getAllExpenses', methods=['GET'])
+@cross_origin(supports_credentials=True)
+@jwt_required()
+def get_all_expenses():
+    expenses = Expense.query.all()
+
+    # Convert the list of expenses to a list of dictionaries
+    expenses_list = [
+        {
+            'id': expense.id,
+            'expense_date': expense.expense_date.strftime('%Y-%m-%d'),  # Convert Date to String
+            'expense_number': expense.expense_number,
+            'invoice_number': expense.invoice_number,
+            'currency': expense.currency,
+            'amount': expense.amount,
+            'notes': expense.notes,
+            'vendor': expense.vendor,
+            'currency': expense.currency,
+            'due_date':expense.due_date,
+            'paid_amount':expense.paid_amount
+        }
+        for expense in expenses
+    ]
+
+    return jsonify({'expenses': expenses_list})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 @app.route("/")
 def home():
